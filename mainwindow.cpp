b9d53f079ff3f4de5a65fd7360c0ff196db6b9c1
luddens <pierre.luddens@epitech.eu>
2019-01-31 21:37:51 +0100

    open file txt in textBrowser

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<QFileDialog>
+#include<QFile>
+#include<QMessageBox>
+#include<QIODevice>
+#include<QTextStream>
 
 mainWindow::mainWindow(QWidget *parent) :
     QWidget(parent),
@@ -17,4 +21,14 @@ mainWindow::~mainWindow()
 void mainWindow::on_pushButton_clicked()
 {
     QString filename = QFileDialog::getOpenFileName(this, "Open file", "", "Text File (*.txt)");
+    if (filename.length() == 0)
+        return;
+    QFile file(filename);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream out(&file);
+        QString data = out.readAll();
+        ui->textBrowser->setText(data);
+    } else {
+        QMessageBox::warning(this, "Error", file.errorString());
+    }
 }
